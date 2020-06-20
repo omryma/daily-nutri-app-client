b93a4940fb78a29dd8e298e74fea8da0c1916c81
omryma <omrymauer@gmail.com>
2020-06-20 16:30:33 +0300

    ui bugs fixing

--- src/components/home.js
+++ src/components/home.js
@@ -30,7 +30,7 @@ const Home = () => {
     <>
       <Spin spinning={isLoading}>
         <Row justify="center" align="top" style={{ direction: 'rtl' }}>
-          <Col flex={1} order={2}><ChartContainer /></Col>
+          <Col xs={24} md={24} lg={9} order={2}><ChartContainer /></Col>
           <Col xs={24} md={24} lg={14} order={1}><MenuContainer /></Col>
         </Row>
       </Spin>
